f00a6e58d0367df79a8fcf9d9df5e17913aee60c
CoraHo <cora9122@gmail.com>
2022-07-13 16:36:00 +0800

    created an exception for duplicated project ID.

--- PPMToolFullStack/ppmtool/src/main/java/io/coraho/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/ppmtool/src/main/java/io/coraho/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.coraho.ppmtool.services;
 
 import io.coraho.ppmtool.domain.Project;
+import io.coraho.ppmtool.exceptions.ProjectIdException;
 import io.coraho.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,10 +13,13 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
 
-        //Logic
-
-        return projectRepository.save(project);
     }
 
 }
