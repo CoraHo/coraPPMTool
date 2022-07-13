0b23da411e422ca6d6dc6a38d2cb69f4cdf74cb0
CoraHo <cora9122@gmail.com>
2022-07-13 15:15:41 +0800

    Fixed the problem that errorMap should be created only when result has error.

--- PPMToolFullStack/ppmtool/src/main/java/io/coraho/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/ppmtool/src/main/java/io/coraho/ppmtool/web/ProjectController.java
@@ -28,12 +28,13 @@ public class ProjectController {
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
-        Map<String, String> errorMap = new HashMap<>();
-        for (FieldError error : result.getFieldErrors()) {
-            errorMap.put(error.getField(), error.getDefaultMessage());
-        }
-
         if (result.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+
+            for (FieldError error : result.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+
             return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
         }
 
