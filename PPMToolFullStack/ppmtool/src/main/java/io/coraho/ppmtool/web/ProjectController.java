248c93ce5a69070254d5e3becedf01f0f36d5afd
CoraHo <cora9122@gmail.com>
2022-07-13 15:55:09 +0800

    Moved errorMap to services folder to make ProjectController to be more concise.

--- PPMToolFullStack/ppmtool/src/main/java/io/coraho/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/ppmtool/src/main/java/io/coraho/ppmtool/web/ProjectController.java
@@ -1,6 +1,7 @@
 package io.coraho.ppmtool.web;
 
 import io.coraho.ppmtool.domain.Project;
+import io.coraho.ppmtool.services.ErrorMapValidationService;
 import io.coraho.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,18 +25,15 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private ErrorMapValidationService errorMapValidationService;
 
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
-        if (result.hasErrors()) {
-            Map<String, String> errorMap = new HashMap<>();
-
-            for (FieldError error : result.getFieldErrors()) {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-
-            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+        ResponseEntity<?> errorMap = errorMapValidationService.ErrorMapValidation(result);
+        if (errorMap != null) {
+            return errorMap;
         }
 
         Project project1 = projectService.saveOrUpdateProject(project);
