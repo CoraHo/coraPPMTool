698d50b03843f59ee1775354ca19365acdc59f40
CoraHo <cora9122@gmail.com>
2022-07-20 16:37:55 +0800

    Updated PPMToolDesign file, installed router and created AddProject.js, CreateProjectButton.js. Updated App.js related addProject and dashboard.

---
+++ ppmtool-react-client/src/components/project/AddProject.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class AddProject extends Component {
+  render() {
+    return (
+      <div>Add Project Form</div>
+    )
+  }
+}
+export default AddProject; 
