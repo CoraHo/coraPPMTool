d92c009d2442cc9e1c0780d676f39c75b3a7f2f3
CoraHo <cora9122@gmail.com>
2022-08-30 16:25:42 -0400

    Connect to MySQL and solved ProjectNOtFound problems for both addProjectTask and getProjectBacklog.

--- PPMToolFullStack/ppmtool/src/main/java/io/coraho/ppmtool/exceptions/CustomResponseEntityExceptionHandler.java
+++ PPMToolFullStack/ppmtool/src/main/java/io/coraho/ppmtool/exceptions/CustomResponseEntityExceptionHandler.java
@@ -14,9 +14,15 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler
-    public final ResponseEntity<Project> handleProjectIDException(ProjectIdException ex, WebRequest request) {
+    public final ResponseEntity<Object> handleProjectIDException(ProjectIdException ex, WebRequest request) {
         ProjectIdExceptionResponse exceptionResponse = new ProjectIdExceptionResponse(ex.getMessage());
         return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
 
+    }
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleProjectNotFound(ProjectNotFoundException ex, WebRequest request) {
+        ProjectIdExceptionResponse exceptionResponse = new ProjectIdExceptionResponse(ex.getMessage());
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
--- PPMToolFullStack/ppmtool/src/main/java/io/coraho/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/ppmtool/src/main/java/io/coraho/ppmtool/services/ProjectTaskService.java
@@ -1,8 +1,11 @@
 package io.coraho.ppmtool.services;
 
 import io.coraho.ppmtool.domain.Backlog;
+import io.coraho.ppmtool.domain.Project;
 import io.coraho.ppmtool.domain.ProjectTask;
+import io.coraho.ppmtool.exceptions.ProjectNotFoundException;
 import io.coraho.ppmtool.repositories.BacklogRepository;
+import io.coraho.ppmtool.repositories.ProjectRepository;
 import io.coraho.ppmtool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,33 +19,48 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
+    @Autowired
+    private ProjectRepository projectRepository;
+
     public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
 
-        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        //project not found
+        try {
+            Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
 
-        projectTask.setBacklog(backlog);
+            projectTask.setBacklog(backlog);
 
-        Integer backlogSequence = backlog.getPTSequence();
+            Integer backlogSequence = backlog.getPTSequence();
 
-        backlogSequence += 1;
+            backlogSequence += 1;
 
-        backlog.setPTSequence(backlogSequence);
+            backlog.setPTSequence(backlogSequence);
 
-        projectTask.setProjectSequence(projectIdentifier + "_" + backlogSequence);
-        projectTask.setProjectIdentifier(projectIdentifier);
+            projectTask.setProjectSequence(projectIdentifier + "_" + backlogSequence);
+            projectTask.setProjectIdentifier(projectIdentifier);
 
-        if (projectTask.getPriority() == null) { // priority is defaulted to be 0.
-            projectTask.setPriority(3);
+            if (projectTask.getPriority() == null) { // priority is defaulted to be 0.
+                projectTask.setPriority(3);
+            }
+
+            if (projectTask.getStatus() == null || projectTask.getStatus() == "") {
+                projectTask.setStatus("To_Do");
+            }
+
+            return projectTaskRepository.save(projectTask);
+        }catch (Exception e) {
+            throw new ProjectNotFoundException("Project Not Found");
         }
 
-        if (projectTask.getStatus() == null || projectTask.getStatus() == "") {
-            projectTask.setStatus("To_Do");
-        }
 
-        return projectTaskRepository.save(projectTask);
     }
 
     public Iterable<ProjectTask> findBacklogById(String id) {
+        Project project = projectRepository.findByProjectIdentifier(id);
+
+        if (project == null) {
+            throw new ProjectNotFoundException("Project with ID:'" + id + "' does not exist");
+        }
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
     }
 }
